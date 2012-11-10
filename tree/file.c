d8a67177ecc885e66a46a7a9cee1289d3c9cf3fb
weiang <weiang@mail.ustc.edu.cn>
2012-11-10 17:05:13 +0800

    modified:   file 	modified:   file.c

--- tree/file.c
+++ tree/file.c
@@ -7,23 +7,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#define	MAX	10
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	FILE *fp;
 	int	i;
 	char	c;
+	int		max;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s <filename> <fileiscale>\n", argv[0]);
 		return 0;
 	}
+	max = atoi(argv[2]);
 	fp = fopen(argv[1], "w");
 	i = 0;
 	srand(time(NULL));
-	while (i < MAX) {
+	while (i < max) {
 		c = rand() % 65 + 32;
 		if (c == 91)
 			i ++;
